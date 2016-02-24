933a4d832790fbd3f3156c75e83ea157c0986687
Maurício Carvalho <maurcarvalho@gmail.com>
2016-02-24 06:03:33 +0100

    indentation fix

--- bob/bob.exs
+++ bob/bob.exs
@@ -1,10 +1,10 @@
 defmodule Bob do
   def hey(input) do
     cond do
-    		String.lstrip(input) |> String.length == 0 -> "Fine. Be that way!"
-    		Regex.match?(~r/[A-Z+А-ЯЁё]{2,}/, input) -> "Whoa, chill out!"
-    	  String.ends_with?(input, "?") -> "Sure."
-        input -> "Whatever."
+     String.lstrip(input) |> String.length == 0 -> "Fine. Be that way!"
+     Regex.match?(~r/[A-Z+А-ЯЁё]{2,}/, input) -> "Whoa, chill out!"
+     String.ends_with?(input, "?") -> "Sure."
+     input -> "Whatever."
     end
   end
 end
